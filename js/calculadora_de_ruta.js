620cc44f2ec89d9bc636d05128f4507e20a25543
da21nny <edgarv21.ev@gmail.com>
2026-01-14 18:59:16 -0300

    nueva clase

---
+++ js/calculadora_de_ruta.js
@@ -0,0 +1,13 @@
+import { AlgoritmoAStar } from "./algoritmo_a_star";
+import { AlgoritmoBusqueda } from "./reconstruir_ruta";
+
+export class CalculadoraDeRuta{
+    constructor(tablero, inicioX, inicioY, finX, finY){
+        this.tablero = tablero;
+        this.inicioX = inicioX;
+        this.inicioY = inicioY;
+        this.finX = finX;
+        this.finY = finY;
+    }
+    
+}
